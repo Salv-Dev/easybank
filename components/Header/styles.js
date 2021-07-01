b96ddfa9dc613a9b059c1a91734827297fc4b15e
Salv-Dev <salv.dev.br@gmail.com>
2021-06-30 21:26:47 -0300

    media queries do header component adicionado

--- components/Header/styles.js
+++ components/Header/styles.js
@@ -6,6 +6,10 @@ export const Container = styled.div`
   align-items: center;
   padding: 25px;
   background-color: hsl(0, 0%, 98%);
+
+  @media(min-width: 800px) {
+    padding: 20px 40px;
+  }
 `;
 
 export const Image = styled.img``;
@@ -14,6 +18,10 @@ export const MenuDesktop = styled.ul`
   display: none;
   justify-content: center;
   list-style: none;
+
+  @media(min-width: 800px) {
+    display: flex;
+  }
 `;
 
 export const Item = styled.li`
@@ -22,6 +30,10 @@ export const Item = styled.li`
     padding: 10px 20px;
     color: hsl(233, 8%, 62%);
     font-size: 0.8rem;
+
+    &:hover, &:active {
+      text-decoration: underline;
+    }
   }
 `;
 
@@ -33,6 +45,10 @@ export const BtnInvite = styled.button`
   color: white;
   background: linear-gradient(90deg, rgba(49,211,92,1) 14%, rgba(43,183,218,1) 100%);
   font-size: 0.7rem;
+
+  @media(min-width: 800px) {
+    display: flex;
+  }
 `;
 
 export const MenuIcon = styled.div`
@@ -41,4 +57,8 @@ export const MenuIcon = styled.div`
   align-items: center;
   width: 20px;
   cursor: pointer;
+
+  @media(min-width: 800px) {
+    display: none;
+  }
 `;
