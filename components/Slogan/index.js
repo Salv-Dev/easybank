86cd0bfd98f5ef1df73f5b2e97322fc32e70839d
Salv-Dev <salv.dev.br@gmail.com>
2021-07-01 01:19:06 -0300

    importando o componente no arquivo index raiz do projeto

--- components/Slogan/index.js
+++ components/Slogan/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 function SloganComponent() {
-  return <div />;
+  return (
+    <Container>
+
+    </Container>
+  );
 }
 
 export default SloganComponent;
