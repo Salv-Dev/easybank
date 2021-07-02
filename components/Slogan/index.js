5591aa3b79242f2bd0fdd214cfd9a4cf0dfb3330
Salv-Dev <salv.dev.br@gmail.com>
2021-07-01 21:11:49 -0300

    texto do slogan adicionado

--- components/Slogan/index.js
+++ components/Slogan/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, SloganImages, SloganText, Title, Text, BtnInvite } from './styles';
 
 function SloganComponent() {
   return (
     <Container>
-        <img src="/bg-intro-mobile.svg" alt="background mobile slogan" className="bg-mobile" />
-        <img src="/image-mockups.png" alt="mobile phone" className="fg-phone" />
+        <SloganImages>
+          <img src="/bg-intro-mobile.svg" alt="background mobile slogan" className="bg-mobile" />
+          <img src="/image-mockups.png" alt="mobile phone" className="fg-phone" />
+        </SloganImages>
+        <SloganText>
+          <Title>Next generation digital banking</Title>
+          <Text>Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.</Text>
+          <BtnInvite>Request Invite</BtnInvite>
+        </SloganText>
     </Container>
   );
 }
--- components/Slogan/styles.js
+++ components/Slogan/styles.js
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const SloganImages = styled.div`
     position: relative;
     display: flex;
 
@@ -14,4 +19,40 @@ export const Container = styled.div`
         width: 100%;
         background: transparent;
     }
+`;
+
+export const SloganText = styled.div`
+    margin-top: -50px;
+    padding: 0 25px;
+    z-index: 2;
+`;
+
+export const Title = styled.h1`
+    font-size: 2rem;
+    font-weight: 300;
+    text-align: center;
+    color: hsl(233, 26%, 24%);
+`;
+
+export const Text = styled.p`
+    font-size: 0.8rem;
+    margin-top: 10px;
+    text-align: center;
+    color: hsl(233, 8%, 62%);
+`;
+
+export const BtnInvite = styled.button`
+  display: flex;
+  border: none;
+  padding: 10px 25px;
+  border-radius: 30px;
+  color: white;
+  background: linear-gradient(90deg, rgba(49,211,92,1) 14%, rgba(43,183,218,1) 100%);
+  font-size: 0.7rem;
+  cursor: pointer;
+  margin: 10px auto;
+
+  &:hover {
+    opacity: 0.5;
+  }
 `;
