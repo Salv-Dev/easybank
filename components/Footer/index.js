42ef74481e03a1aadfe05fbe5e7eccbae3db912f
Salv-Dev <salv.dev.br@gmail.com>
2021-07-10 18:56:20 -0300

    estrutura e estilos iniciais

---
+++ components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function FooterComponent() {
+  return (
+      <Container></Container>
+  );
+}
+
+export default FooterComponent;
